71a28b2007426cec838c46684216529cc8e3688f
asmasud500 <110842433+asmasud500@users.noreply.github.com>
2026-06-09 06:59:13 +0600

    Fix: app/src/main/java/com/iptvplayer/IPTVApp.kt

--- app/src/main/java/com/iptvplayer/IPTVApp.kt
+++ app/src/main/java/com/iptvplayer/IPTVApp.kt
@@ -2,18 +2,10 @@ package com.iptvplayer
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
 
 @HiltAndroidApp
 class IPTVApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        initializeLogging()
-    }
-
-    private fun initializeLogging() {
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
     }
 }
